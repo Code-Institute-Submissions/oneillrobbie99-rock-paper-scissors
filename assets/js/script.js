3d4923bf4f9221c864787edd513e24b89327c4c0
oneillrobbie99 <oneillrobbie99@gmail.com>
2022-01-26 15:29:37 +0000

    assigned choice values to numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,8 @@ const buttonChoices = document.querySelectorAll('button')
 
 let userPick
 
+let computerPicks
+
 buttonChoices.forEach(buttonChoice => buttonChoice.addEventListener('click', (e) =>{
     userPick=e.target.id
     playerPick.innerHTML = userPick
@@ -18,4 +20,16 @@ buttonChoices.forEach(buttonChoice => buttonChoice.addEventListener('click', (e)
 function randomComputerPick(){
     const randomPick = Math.floor(Math.random() * buttonChoices.length)
     console.log(randomPick)
+}
+
+if (computerPicks === 1) {
+    computerPicks = 'paper'
+}
+
+if (computerPicks === 2) {
+    computerPicks = 'scissors'
+}
+
+if (computerPicks === 3) {
+    computerPicks = 'rock'
 }
